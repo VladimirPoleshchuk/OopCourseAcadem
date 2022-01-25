9c664b4622cd1e4e82194a3fb4f951c0872c4483
vladimir Pol <vladipoleh@gmail.com>
2022-01-26 00:58:26 +0700

    Дбавил метод GetIntersection

--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -1,17 +1,25 @@
-﻿using System;
-
-namespace RangeTask
+﻿namespace RangeTask
 {
     public class Range
     {
-         public double From { get; set; }
+        public double From { get; set; }
 
         public double To { get; set; }
+
+        public Range Range1 { get; set; }
+
+        public Range Range2 { get; set; }
 
         public Range(double from, double to)
         {
             From = from;
             To = to;
+        }
+
+        public Range(Range range1, Range range2)
+        {
+            Range1 = range1;
+            Range2 = range2;
         }
 
         public double GetLength()
@@ -21,7 +29,47 @@ namespace RangeTask
 
         public bool IsInside(double number)
         {
-            return (number - From >0 && number - To <0);
+            return (number - From > 0 && number - To < 0);
+        }
+
+        public Range GetIntersection()
+        {
+            if (Range1.To < Range2.From || Range2.To < Range1.From)
+            {
+                return null;
+            }
+
+            if (Range1.From<Range2.From && Range1.To>Range2.To)
+            {
+                return new Range(Range2.From, Range2.To);
+            }
+
+            if (Range1.From > Range2.From && Range1.To < Range2.To)
+            {
+                return new Range(Range1.From, Range1.To);
+            }
+
+            if (Range1.From < Range2.From && Range1.To > Range2.From)
+            {
+                return new Range(Range2.From, Range1.To);
+            }
+
+            if (Range2.From < Range1.From && Range1.From < Range2.To)
+            {
+                return new Range(Range1.From, Range2.To);
+            }
+            
+            return Range1;
+        }
+
+        public override string ToString()
+        {
+            //if (intersection == null)
+            //{
+            //    return "Интервала-пересечения нет.";
+            //}
+
+            return String.Format("Интервал-пересечения двух интервалов: ({0:f2}; {1:f2})", From, To);                       
         }
     }
 }
--- RangeTask/RangeTask.cs
+++ RangeTask/RangeTask.cs
@@ -1,8 +1,6 @@
-﻿using System;
-
-namespace RangeTask 
+﻿namespace RangeTask
 {
-   public class RangeTask
+    public class RangeTask
     {
         static void Main()
         {
@@ -16,14 +14,12 @@ namespace RangeTask
             Console.Write("Введите предполагаемое вещественное число данного диапазона: ");
             double possibleNumber = Convert.ToDouble(Console.ReadLine());
 
-            Console.Clear();
-
-            Range numbersRange = new Range(fromNumber, toNumber);
+            Range range = new Range(fromNumber, toNumber);
 
             Console.Write($"Длинна диапазона чисел {fromNumber} и {toNumber}: ");
-            Console.WriteLine("{0:f5}", numbersRange.GetLength());
+            Console.WriteLine("{0:f2}", range.GetLength());
 
-            if (numbersRange.IsInside(possibleNumber))
+            if (range.IsInside(possibleNumber))
             {
                 Console.WriteLine($"Число {possibleNumber} в заданном диапазоне вещественных чисел!");
             }
@@ -31,6 +27,46 @@ namespace RangeTask
             {
                 Console.WriteLine($"Число {possibleNumber} не в заданном диапазоне вещественных чисел!");
             }
+
+            Console.WriteLine("Нажмите любую клавишу для продолжения работы программы.");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Введите два диапазона вещественных чисел для  которых надо определить интервала-пересечения, объдинения и разности.");
+            Console.Write("Введите начальное число первого диапазона: ");
+            double fromNumber1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите конечное число первого диапазона: ");
+            double toNumber1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите начальное число второго диапазона: ");
+            double fromNumber2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите конечное число второго диапазона: ");
+            double toNumber2 = Convert.ToDouble(Console.ReadLine());
+
+            Range range1 = new Range(fromNumber1, toNumber1);
+
+            Range range2 = new Range(fromNumber2, toNumber2);
+
+            Range intersectionRange = new(range1, range2);
+
+            Range intersection =intersectionRange.GetIntersection();
+
+            Console.WriteLine(intersection.ToString());
+
+            //if (intersection == null)
+            //{
+            //    Console.WriteLine("Интервала-пересечения нет.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Интервал-пересечения двух интервалов: ({0:f2}; {1:f2})", intersection.From, intersection.To);
+            //}
+
+            //Range unionRange = GetUnion(numbersRange1, numbersRange2);
+
+            //Range differenceRange = GetDifference(numbersRange1, numbersRange2);
         }
     }
 }
