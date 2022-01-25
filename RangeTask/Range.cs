a8c2b7ad0aa4ffa9c41812f361bf0269828d0469
vladimir Pol <vladipoleh@gmail.com>
2022-01-25 21:36:31 +0700

    Добавьте файлы проекта.

---
+++ RangeTask/Range.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RangeTask
+{
+    public class Range
+    {
+         public double From { get; set; }
+
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return (number - From >0 && number - To <0);
+        }
+    }
+}
---
+++ RangeTask/RangeTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RangeTask 
+{
+   public class RangeTask
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите диапазон вещественных чисел и число которое надо определить на этом диапазоне.");
+            Console.Write("Введите начальное число диапазона: ");
+            double fromNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите конечное число диапазона: ");
+            double toNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите предполагаемое вещественное число данного диапазона: ");
+            double possibleNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Clear();
+
+            Range numbersRange = new Range(fromNumber, toNumber);
+
+            Console.Write($"Длинна диапазона чисел {fromNumber} и {toNumber}: ");
+            Console.WriteLine("{0:f5}", numbersRange.GetLength());
+
+            if (numbersRange.IsInside(possibleNumber))
+            {
+                Console.WriteLine($"Число {possibleNumber} в заданном диапазоне вещественных чисел!");
+            }
+            else
+            {
+                Console.WriteLine($"Число {possibleNumber} не в заданном диапазоне вещественных чисел!");
+            }
+        }
+    }
+}
